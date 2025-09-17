6337c19a6b157fed0a083c2b3a08ef560fd43e60
Meriem <mimi.04.05.2003@gmail.com>
2025-09-18 00:24:05 +0100

    Update utils.ts

--- Client/src/utils.ts
+++ Client/src/utils.ts
@@ -1,3 +1,4 @@
 // generate local hose url and export it
-export const localHostUrl = "https://lucid-optimism-production.up.railway.app/api/v1";
-export const Url = "https://baridi-trust.up.railway.app";
+const localHostUrl = "https://lucid-optimism-production.up.railway.app/api/v1";
+const Url = "https://baridi-trust.up.railway.app";
+export {localHostUrl,Url};
