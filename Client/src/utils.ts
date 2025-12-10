54e1b4587d3e361bc1ba0e512a63becfe3a2cb65
Meriem ZEMANE <mimi.04.05.2003@gmail.com>
2025-12-10 23:58:43 +0100

    Update utils.ts

--- Client/src/utils.ts
+++ Client/src/utils.ts
@@ -1,4 +1,4 @@
 // generate local hose url and export it
-const localHostUrl = "https://lucid-optimism-production.up.railway.app/api/v1";
-const Url = "https://baridi-trust.up.railway.app";
+const localHostUrl = "https://baridi-trust-1.onrender.com/api/v1";
+const Url = "https://baridi-trust.onrender.com";
 export {localHostUrl,Url};
